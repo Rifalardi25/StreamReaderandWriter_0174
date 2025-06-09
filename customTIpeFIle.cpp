6ed97b2697096bf856e415b19ae924012246257d
RifalArdi25 <rifalardi25@gmail.com>
2025-06-09 15:57:04 +0700

    Membuat fungsi readandwritefile

--- customTIpeFIle.cpp
+++ customTIpeFIle.cpp
@@ -3,5 +3,53 @@
 #include <string>
 using namespace std;
 
+void readandwritefile(string format){
+    string baris, nameFile;
+
+    cout << "Masukan name file : ";
+    cin >> nameFile;
+    cin.ignore();
+
+    // Membaut objek output file stream
+    ofstream outfile;
+    // Membuka file untuk ditulisi.
+    outfile.open(nameFile + format);
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    // Unlimited loop untuk menulis
+    while(true){
+        cout << "- ";
+        // Mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // Loop akan berhanti jika anda memasuakn karakter q
+        if (baris == "q")break;
+        // Menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // Mmbuat objek input file stream
+    ifstream infile;
+
+    // Mmebuak file yang telah ditulis
+    infile.open(nameFile + format);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // Jika file ada maka
+    if (infile.is_open()){
+        // Melakukan perulanagn setiap baris
+        while (getline(infile, baris)){
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // Jika tidak ditemukan file makan akan menampilkan ini
+    else cout << "Unable to open file";
+}
+
 
 
