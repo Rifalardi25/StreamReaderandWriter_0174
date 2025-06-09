14801dbbc92c430c694d9e9819413ab1f986b919
RifalArdi25 <rifalardi25@gmail.com>
2025-06-09 15:22:50 +0700

    Menambahkan try, catch int a, catch default

--- manualEception.cpp
+++ manualEception.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 3.5; // Melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        // Blok ini akan dieksekusi
+        cout << a << " : Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*Jika selain iteger maka block ini akan dieksekusi*/
+        cout << "Default Pengecualian dieksekusi" << endl;
+    }
 
+    return 0;
 }
